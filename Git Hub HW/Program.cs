4e9bb3b59aedfeea747eae8641a35d7ed0ebf169
Mason <Mason@DESKTOP-NHDIDLB>
2021-01-25 18:22:22 -0600

    This is the new message with the game

--- Git Hub HW/Program.cs
+++ Git Hub HW/Program.cs
@@ -11,7 +11,28 @@ namespace Git_Hub_HW
         static void Main(string[] args)
         {
             Console.WriteLine("Hellow World!");
-            
+            Console.WriteLine("I am thinking of a number betwen 0 - 100");
+            Random rnd = new Random();
+            int num = rnd.Next(1, 101);
+            int i = 1;
+            Console.WriteLine("What is your guess?");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            while (choice != num)
+            {
+                if (choice < num)
+                {
+                    Console.WriteLine("Ooh too bad your number was too low, try again.");
+                }
+                if (choice > num)
+                {
+                    Console.WriteLine("oof too bad your number was too high, try again");
+                }
+                i++;
+                choice = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Correct the number was: " + num);
+            Console.WriteLine("That took you " + i + " guesses");
+            Console.ReadKey();
         }
     }
 }
